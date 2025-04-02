d2c5f13a6f9074404c8b8a88df1370959ab7f14c
Izzet <yunusovizzet6@gmail.com>
2025-04-02 07:09:04 +0200

    changed

--- Calculator_test.py
+++ Calculator_test.py
@@ -1,29 +1,29 @@
+# # from calculator import Calculator
+# #
+# # cal = Calculator()
+# #
+# # # тестируем метод sum
+# #
+# # res = cal.sum(4, 6)
+# # assert res == 10, 'результат не совпал'
+# #
+# # res1 = cal.sum(-4, -2)
+# # assert res1 == -6, 'результат не совпал'
+# #
+# # res2 = cal.avg([1,2,3])
+# # assert res2 == 2, 'результат не совпал'
+#
+#
 # from calculator import Calculator
 #
-# cal = Calculator()
+# import pytest
 #
-# # тестируем метод sum
+# @pytest.fixture
+# def calculator():
+#     return Calculator()
 #
-# res = cal.sum(4, 6)
-# assert res == 10, 'результат не совпал'
+# def test_sum_pos_muns(calculator):
+#     assert calculator.sum(1, 2) == 3
 #
-# res1 = cal.sum(-4, -2)
-# assert res1 == -6, 'результат не совпал'
-#
-# res2 = cal.avg([1,2,3])
-# assert res2 == 2, 'результат не совпал'
-
-
-from calculator import Calculator
-
-import pytest
-
-@pytest.fixture
-def calculator():
-    return Calculator()
-
-def test_sum_pos_muns(calculator):
-    assert calculator.sum(1, 2) == 3
-
-def test_sum_neg_muns(calculator):
-    assert calculator.sum(-1, -2) == -3
+# def test_sum_neg_muns(calculator):
+#     assert calculator.sum(-1, -2) == -3
---
+++ homeworks/hw1/test_simple_math.py
@@ -0,0 +1,16 @@
+# from hw1.simple_math import SimpleMath
+#
+# import pytest
+#
+# @pytest.fixture
+# def simple_math():
+#     return SimpleMath()
+#
+# def test_square(simple_math):
+#     assert simple_math.square(2) == 4
+#     assert simple_math.square(0) == 0
+#     assert simple_math.square(-7) == 49
+#
+# def test_cube(simple_math):
+#     assert simple_math.cube(-3) == -27
+#     assert simple_math.cube(0) == 0
