d824e46d8f8a100e75593fda51ac5f85f13d5d66
Izzet <yunusovizzet6@gmail.com>
2025-04-02 07:09:23 +0200

    hw3

---
+++ homeworks/hw3_test.py
@@ -0,0 +1,71 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+
+
+@pytest.fixture
+def web_driver():
+    driver = webdriver.Chrome()
+    driver.get("https://itcareerhub.de/ru")
+    yield driver
+    driver.quit()
+
+
+
+def test_task1(web_driver):
+    """Тест проверяет отображение элементов на странице ITCareerHub."""
+
+    def check_element(by, value, description):
+        """Проверяет, отображается ли элемент, и выводит результат."""
+        time.sleep(1)
+        try:
+            element = web_driver.find_element(by, value)
+            assert element.is_displayed()
+            print(f"✅ {description} найден")
+        except NoSuchElementException:
+            print(f"❌ {description} НЕ найден")
+        except AssertionError:
+            print(f"⚠️ {description} найден, но не отображается")
+
+    # Проверка логотипа
+    check_element(
+        By.XPATH,
+        '//*[@class="tn-atom__img t-img loaded"]', "Логотип ITCareerHub"
+    )
+
+    # Проверка ссылок в меню
+    menu_links = [
+        ("Программы", By.LINK_TEXT, "Ссылка 'Программы'"),
+        ("Способы оплаты", By.LINK_TEXT, "Ссылка 'Способы оплаты'"),
+        ("Новости", By.LINK_TEXT, "Ссылка 'Новости'"),
+        ("О нас", By.LINK_TEXT, "Ссылка 'О нас'"),
+        ("Отзывы", By.LINK_TEXT, "Ссылка 'Отзывы'"),
+    ]
+
+    for text, by, description in menu_links:
+        check_element(by, text, description)
+
+    # Проверка кнопок переключения языка
+    check_element(By.CLASS_NAME, "tn-atom", "Кнопка переключения языка")
+
+
+
+# 3. Кликнуть по иконке с телефонной трубкой
+# 4. Проверить что текст “Если вы не дозвонились, заполните форму на сайте. Мы свяжемся с вами” отображается.
+def test_click_phone_icon(web_driver):
+    time.sleep(2)
+    phone_icon = web_driver.find_element(By.XPATH, "//img[@class='tn-atom__img t-img loaded']")
+    phone_icon.click()
+
+    time.sleep(1)
+
+    # Проверяем наличие текста
+    text_element = web_driver.find_element(
+        By.XPATH,
+        "//div[@field='tn_text_1711363912027']"
+    )
+    assert text_element
+
