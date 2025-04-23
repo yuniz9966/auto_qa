d3a07d3721dfc5c796bf34c2651c828fe8002813
Izzet <yunusovizzet6@gmail.com>
2025-04-23 15:49:06 +0200

    changed hw3

--- homeworks/hw3_test.py
+++ homeworks/hw3_test.py
@@ -2,8 +2,6 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
-from conftest import driver
-
 
 
 def test_task1(driver):
@@ -11,7 +9,6 @@ def test_task1(driver):
 
     def check_element(by, value, description):
         """Проверяет, отображается ли элемент, и выводит результат."""
-        time.sleep(1)
         try:
             element = driver.find_element(by, value)
             assert element.is_displayed()
@@ -47,11 +44,9 @@ def test_task1(driver):
 # 3. Кликнуть по иконке с телефонной трубкой
 # 4. Проверить что текст “Если вы не дозвонились, заполните форму на сайте. Мы свяжемся с вами” отображается.
 def test_click_phone_icon(driver):
-    time.sleep(2)
     phone_icon = driver.find_element(By.XPATH, "//img[@class='tn-atom__img t-img loaded']")
     phone_icon.click()
 
-    time.sleep(1)
 
     # Проверяем наличие текста
     text_element = driver.find_element(
