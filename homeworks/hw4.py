3dc170236ed958eebd0d5460ab21c2806707ee8b
Izzet <yunusovizzet6@gmail.com>
2025-04-23 15:49:47 +0200

    changed hw4

--- homeworks/hw4.py
+++ homeworks/hw4.py
@@ -2,23 +2,21 @@ import pytest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from conftest import driver
 
 
 
-# def test_text_input(driver):
-#     driver.get("http://uitestingplayground.com/textinput")
-#     text_field = driver.find_element(By.CLASS_NAME, "form-control")
-#     text_field.click()
-#     text_field.send_keys("ITCH")
-#     execute_button = driver.find_element(By.CLASS_NAME, "btn-primary")
-#     execute_button.click()
-#
-#     assert execute_button.text == "ITCH", "Тест не прошел"
+def test_text_input(driver):
+    driver.get("http://uitestingplayground.com/textinput")
+    text_field = driver.find_element(By.CLASS_NAME, "form-control")
+    text_field.click()
+    text_field.send_keys("ITCH")
+    execute_button = driver.find_element(By.CLASS_NAME, "btn-primary")
+    execute_button.click()
+
+    assert execute_button.text == "ITCH", "Тест не прошел"
 
 
-# def test_check_image(driver):
-#     driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
-#     time.sleep(10)
-#     image_locator = driver.find_element(By.XPATH, '//img[@alt="award"]')
-#     assert image_locator.get_attribute("alt") == "award"
+def test_check_image(driver):
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+    image_locator = driver.find_element(By.XPATH, '//img[@alt="award"]')
+    assert image_locator.get_attribute("alt") == "award"
